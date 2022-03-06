bd7ac6a74b8929b580e896646fdd54d206b68e5e
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-03-07 01:18:00 +0300

    Update Main.kt
    - added matrix multiplication by constant.

--- Main.kt
+++ Main.kt
@@ -1,6 +1,28 @@
 package processor
 
 fun main() {
+    matrixMultiplicationByConstant()
+//    matrixAddition()
+}
+
+fun matrixMultiplicationByConstant() {
+    val input = readLine()!!.split(" ").map { it.toInt() }
+    val matrix = mutableListOf<List<Int>>()
+    repeat(input[0]) {
+        val line = readLine()!!.split(" ").map { it.toInt() }
+        matrix.add(line)
+    }
+    val const = readLine()!!.toInt()
+    for (line in matrix) {
+        val result = mutableListOf<Int>()
+        for (int in line) {
+            result.add(int * const)
+        }
+        println(result.joinToString(" "))
+    }
+}
+
+private fun matrixAddition() {
     val firstMatrixParams = readLine()!!.split(" ").map { it.toInt() }
     val firstMatrix = mutableListOf<List<Int>>()
     repeat(firstMatrixParams[0]) {
